71408fda5dc3c4f0be006f60752aaa92c5f25ad2
vitusverden <37027595+vitusverden@users.noreply.github.com>
2019-04-16 11:35:39 +0200

    Alt virker nu.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var today = new Date()
 Date.prototype.getWeekNumber = function(){
     var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
     var dayNum = d.getUTCDay() || 7;
@@ -7,10 +8,25 @@ Date.prototype.getWeekNumber = function(){
   };
   
 function getRealWeekNumber() {
-    return new Date().getWeekNumber();
+    return today.getWeekNumber();
 }
 
 function getRollespil() {
-    
+	if (today.getDay() == 5) {
+		document.getElementById("rollespil?").textContent = "Ja, det er jo fredag."
+	} else if (today.getDay() == 4 && isDivisible(getRealWeekNumber(), 2)) {
+		document.getElementById("rollespil?").textContent = "Ja, det er en lige torsdag."
+	} else if (today.getDay() == 2) {
+		document.getElementById("rollespil?").textContent = "Måske, har Emil tid?"
+	} else {
+		document.getElementById("rollespil?").textContent = "Nej-"
+	}
 }
 
+function isDivisible(number, by) {
+	if (number / by == Math.floor(number / by)) {
+		return true
+	} else {
+		return false
+	}
+}
